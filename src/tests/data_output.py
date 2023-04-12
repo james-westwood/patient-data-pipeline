becb3637ad978e56c0fe201c50a3b22805147720
James Westwood <mrjameswestwood@gmail.com>
2023-04-12 19:28:16 +0000

    moving do

--- src/tests/data_output.py
+++
@@ -1,4 +0,0 @@
-# define a function to write duckdb.DuckDBPyRelation out to parquet
-def write_to_parquet(db_obj, outpath=data_out):
-    "Write duckdb.DuckDBPyRelation to parquet."
-    db_obj.df().to_parquet(outpath)
