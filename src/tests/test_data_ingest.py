1afc4635b036cf4c81975fbb5b4de95bedd19efc
James Westwood <mrjameswestwood@gmail.com>
2023-04-17 08:39:52 +0000

    Unit tests

--- src/tests/test_data_ingest.py
+++ src/tests/test_data_ingest.py
@@ -1,3 +1,46 @@
 """
     Test scripts for data_ingest
 """
+
+from src.data_ingest import read_patient_records
+
+def test_read_patient_records():
+    # Define a temporary JSON file with test data
+    import tempfile
+    import json
+    
+    with tempfile.NamedTemporaryFile(mode='w', delete=False) as f:
+        f.write(json.dumps({
+            "entry": [{
+                "resource": {
+                    "id": "1",
+                    "name": {"family": "Doe", "given": ["John"]},
+                    "gender": "male",
+                    "birthDate": "1970-01-01"
+                }
+            }]
+        }))
+        test_file = f.name
+
+    # Call the function with the temporary file
+    result = read_patient_records([test_file])
+
+    # Check that the result is a list with one item
+    assert isinstance(result, list)
+    assert len(result) == 1
+
+    # Check that the item is a dictionary with the expected keys
+    patient_json = result[0]
+    assert isinstance(patient_json, dict)
+    assert set(patient_json.keys()) == {"id", "name", "gender", "birthDate"}
+
+    # Check that the values are correct
+    assert patient_json["id"] == "1"
+    assert patient_json["name"]["family"] == "Doe"
+    assert patient_json["name"]["given"] == ["John"]
+    assert patient_json["gender"] == "male"
+    assert patient_json["birthDate"] == "1970-01-01"
+
+    # Remove the temporary file
+    import os
+    os.unlink(test_file)
---
+++ src/tests/test_database_mods.py
@@ -0,0 +1,29 @@
+import pytest
+import duckdb
+from src.database_mod import read_parquet, check_parquet
+
+@pytest.fixture(scope="module")
+def test_db():
+    """Create a temporary in-memory database for testing."""
+    db = duckdb.connect(':memory:')
+    yield db
+    db.close()
+
+def test_read_parquet(test_db):
+    """Test that read_parquet returns a valid DuckDBPyRelation."""
+    parquet_file = "src/tests/test.parquet"
+    db_obj = read_parquet(parquet_file=parquet_file)
+    assert isinstance(db_obj, duckdb.DuckDBPyRelation)
+
+def test_check_parquet_exists():
+    """Test that check_parquet returns True if the Parquet file exists."""
+    parquet_file = "src/tests/test.parquet"
+    exists = check_parquet(check_path=parquet_file)
+    assert exists == True
+    
+def test_check_parquet_not_exists():
+    """Test that check_parquet returns False if the Parquet file does not exist."""
+    parquet_file = "src/tests/fake.parquet"
+    exists = check_parquet(check_path=parquet_file)
+    assert exists == False
+
