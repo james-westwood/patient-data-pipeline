dda52a12ef8bc518116e83e2ffe337eb0b7095e7
James Westwood <mrjameswestwood@gmail.com>
2023-04-17 14:57:49 +0000

    Change to root dir

--- src/tests/test_database_mods.py
+++ src/tests/test_database_mods.py
@@ -4,8 +4,8 @@ from src.database_mod import read_parquet, check_parquet
 import os
 
 
-print(f"This is the CWD : {os.getcwd()} !!!!!!!!!!!!!!!!!!!!!!!!")
-print(os.environ)
+# Change working directory to the root of the project
+os.chdir(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 @pytest.fixture(scope="module")
 def test_db():
